01c0a02d6e485bbea1670aa2cd735e5fc017e1af
Ahmad Choiruddin <83292508+choir94@users.noreply.github.com>
2024-08-17 19:41:05 +0700

    Create logo.sh

---
+++ logo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -e "\e[1;32m"
+echo -e "        LOADING"
+echo -e ""
+echo -e "                 \033[48;2;9;10;12m Airdrop Node \e[0m"
+echo -e "\e[0;37m Join Telegram Channel: \e[4;35mhttps://t.me/airdrop_node/\e[0m"
+echo -e "\e[0m"
+echo -e ""
+echo -e ""
