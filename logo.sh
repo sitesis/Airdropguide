6fccbabb181384ed777abbca5be89c562c47039f
Ahmad Choiruddin <83292508+choir94@users.noreply.github.com>
2024-09-24 21:07:57 +0700

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-echo "####################################################"
-echo "####################################################"
-echo "##                                               ###"
-echo "##                AIRDROP NODE                   ###"
-echo "## Join My Telegram: https://t.me/airdrop_node   ###"
-echo "##                                               ###"
-echo "####################################################"
-echo "####################################################"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+echo -e "${BLUE}####################################################${NC}"
+echo -e "${BLUE}##                                                ##${NC}"
+echo -e "${YELLOW}##               🌟 AIRDROP NODE 🌟                ##${NC}"
+echo -e "${GREEN}##        Join My Telegram:                       ##${NC}"
+echo -e "${GREEN}##         https://t.me/airdrop_node              ##${NC}"
+echo -e "${BLUE}##                                                ##${NC}"
+echo -e "${BLUE}####################################################${NC}"
