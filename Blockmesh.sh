89dbeb5ab4256e93204fccbb650733cabc7012d1
Ahmad Choiruddin <83292508+choir94@users.noreply.github.com>
2024-10-30 19:22:51 +0700

    Update Blockmesh.sh

--- Blockmesh.sh
+++ Blockmesh.sh
@@ -7,8 +7,9 @@ GREEN='\033[1;32m'
 UNDERLINE_YELLOW='\033[1;4;33m'
 NC='\033[0m' # No Color
 
-# Display logo
-echo -e "${YELLOW}
+# Function to display logo
+display_logo() {
+    echo -e "${YELLOW}
            _         _                   _   _           _      
      /\   (_)       | |                 | \ | |         | |     
     /  \   _ _ __ __| |_ __ ___  _ __   |  \| | ___   __| | ___ 
@@ -22,6 +23,7 @@ ${BLUE}
         ──────────────────────────────────────
         🚀 Telegram Group: ${UNDERLINE_YELLOW}https://t.me/airdrop_node${NC}
         ──────────────────────────────────────"
+}
 
 # Set directory name
 DIRECTORY="blockmesh_directory"
@@ -110,11 +112,40 @@ view_logs() {
     read -p "Tekan Enter untuk kembali ke menu..."
 }
 
+# Function to restart node
+restart_node() {
+    echo "Memulai ulang node..."
+    docker-compose down
+    docker-compose up -d
+    echo "Node telah dimulai ulang."
+    read -p "Tekan Enter untuk kembali ke menu..."
+}
+
+# Function to stop node
+stop_node() {
+    echo "Menghentikan node..."
+    docker-compose down
+    echo "Node telah dihentikan."
+    read -p "Tekan Enter untuk kembali ke menu..."
+}
+
 # Function to start node
 start_node() {
     echo "Memulai node..."
     docker-compose up -d
     echo "Node telah dimulai."
+    read -p "Tekan Enter untuk kembali ke menu..."
+}
+
+# Function to change account details
+change_account() {
+    echo "Mengubah detail akun..."
+    read -p "Masukkan email baru: " USER_EMAIL
+    read -sp "Masukkan kata sandi baru: " USER_PASSWORD
+    echo
+    echo "USER_EMAIL=${USER_EMAIL}" > .env
+    echo "USER_PASSWORD=${USER_PASSWORD}" >> .env
+    echo "Detail akun berhasil diperbarui."
     read -p "Tekan Enter untuk kembali ke menu..."
 }
 
@@ -127,13 +158,17 @@ cat_account() {
 # Main menu
 show_menu() {
     clear
+    display_logo  # Call the function to display the logo
     echo "Silakan pilih opsi:"
     echo "1.  Instal Node"
     echo "2.  Lihat Log"
-    echo "3.  Mulai Node"
+    echo "3.  Restart Node"
+    echo "4.  Hentikan Node"
+    echo "5.  Mulai Node"
     echo "6.  Lihat Akun"
+    echo "7.  Ganti Akun"
     echo "0.  Keluar"
-    echo -n "Masukkan nomor perintah [0-3]: "
+    echo -n "Masukkan nomor perintah [0-7]: "
     read -r choice
 }
 
@@ -145,8 +180,11 @@ while true; do
     case $choice in
         1) install_node ;;
         2) view_logs ;;
-        3) start_node ;;
+        3) restart_node ;;
+        4) stop_node ;;
+        5) start_node ;;
         6) cat_account ;;
+        7) change_account ;;
         0) echo "Keluar..."; exit 0 ;;
         *) echo "Input tidak valid. Silakan coba lagi."; read -p "Tekan Enter untuk melanjutkan..." ;;
     esac
