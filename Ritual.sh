01e411859c729610b100716459d5b28476d71ac9
Ahmad Choiruddin <83292508+choir94@users.noreply.github.com>
2024-11-04 13:20:38 +0700

    Update Ritual.sh

--- Ritual.sh
+++ Ritual.sh
@@ -1,63 +1,62 @@
 #!/bin/bash
 
+# Warna
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+NC='\033[0m' # Tanpa warna
+
 # Memeriksa apakah skrip dijalankan sebagai pengguna root
 if [ "$(id -u)" != "0" ]; then
-    echo "Skrip ini memerlukan hak akses root."
-    echo "Silakan coba menggunakan perintah 'sudo -i' untuk beralih ke pengguna root, lalu jalankan skrip ini lagi."
+    echo -e "${RED}Skrip ini memerlukan hak akses root.${NC}"
+    echo "Silakan coba dengan perintah 'sudo -i' untuk masuk sebagai pengguna root, lalu jalankan skrip ini lagi."
     exit 1
 fi
 
 # Jalur penyimpanan skrip
 SCRIPT_PATH="$HOME/Ritual.sh"
 
-# Fungsi untuk meminta input pengguna dan validasi
-function prompt_input() {
-    local prompt_message=$1
-    local variable_name=$2
-    local regex=$3
-
-    while true; do
-        read -p "$prompt_message" input_value
-        if [[ $input_value =~ $regex ]]; then
-            eval "$variable_name=\"$input_value\""
-            break
-        else
-            echo "Input tidak valid. Silakan coba lagi."
-        fi
-    done
-}
-
-# Fungsi instalasi node
+# Fungsi untuk menginstal node
 function install_node() {
-    # Meminta pengguna memasukkan private_key dan melakukan validasi
-    prompt_input "Masukkan private key EVM wallet (0x...): " private_key "^(0x)[0-9a-fA-F]{40}$"
-    prompt_input "Masukkan alamat wallet yang sesuai (0x...): " wallet_address "^(0x)[0-9a-fA-F]{40}$"
-    prompt_input "Masukkan RPC, harus menggunakan Base chain: " rpc_address "^https?://.+"
-    prompt_input "Masukkan port: " port1 "^[0-9]+$"
+
+    # Meminta pengguna untuk memasukkan private_key
+    read -p "Masukkan private key dompet EVM, harus dimulai dengan 0x, disarankan menggunakan dompet baru: " private_key
+    read -p "Masukkan alamat dompet yang sesuai, harus dimulai dengan 0x, disarankan menggunakan dompet baru: " wallet_address
+    # Meminta pengguna untuk memasukkan RPC address
+    read -p "Masukkan RPC, harus di jaringan Base: " rpc_address
+    # Meminta pengguna untuk memasukkan port
+    read -p "Masukkan port: " port1
 
     # Memperbarui daftar paket sistem
-    echo "Memperbarui daftar paket sistem..."
     sudo apt update
 
-    # Memeriksa apakah Git telah diinstal
-    if ! command -v git &> /dev/null; then
-        echo "Git tidak terdeteksi, sedang menginstal..."
-        sudo apt install git -y || { echo "Instalasi Git gagal."; exit 1; }
+    # Memeriksa apakah Git sudah terinstal
+    if ! command -v git &> /dev/null
+    then
+        # Jika Git belum terinstal, lakukan instalasi
+        echo -e "${YELLOW}Git tidak terdeteksi, sedang menginstal...${NC}"
+        sudo apt install git -y
     else
-        echo "Git telah terinstal."
+        # Jika Git sudah terinstal, tidak melakukan tindakan
+        echo -e "${GREEN}Git sudah terinstal.${NC}"
     fi
 
     # Mengkloning repositori ritual-net
-    git clone https://github.com/ritual-net/infernet-node || { echo "Gagal mengkloning repositori."; exit 1; }
-    cd infernet-node || { echo "Gagal masuk ke direktori infernet-node."; exit 1; }
+    git clone https://github.com/ritual-net/infernet-node
 
-    # Menetapkan tag dan membangun gambar Docker
+    # Masuk ke direktori infernet-deploy
+    cd infernet-node
+
+    # Menetapkan tag
     tag="v1.0.0"
-    echo "Membangun gambar Docker..."
-    docker build -t ritualnetwork/infernet-node:$tag . || { echo "Gagal membangun gambar Docker."; exit 1; }
 
-    # Masuk ke direktori deploy
-    cd deploy || { echo "Gagal masuk ke direktori deploy."; exit 1; }
+    # Membangun gambar
+    docker build -t ritualnetwork/infernet-node:$tag .
+
+    # Masuk ke direktori
+    cd deploy
 
     # Menggunakan perintah cat untuk menulis konfigurasi ke config.json
     cat > config.json <<EOF
@@ -65,7 +64,7 @@ function install_node() {
   "log_path": "infernet_node.log",
   "manage_containers": true,
   "server": {
-    "port": $port1,
+    "port": 4000,
     "rate_limit": {
       "num_requests": 100,
       "period": 100
@@ -88,6 +87,9 @@ function install_node() {
       "batch_size": 800,
       "sync_period": 30
     }
+  },
+  "docker": {
+    "username": "username"
   },
   "redis": {
     "host": "redis",
@@ -117,15 +119,16 @@ function install_node() {
 }
 EOF
 
-    echo "File konfigurasi telah disiapkan."
+    echo -e "${GREEN}File konfigurasi telah disiapkan.${NC}"
 
     # Menginstal komponen dasar
-    echo "Menginstal komponen dasar..."
-    sudo apt install pkg-config curl build-essential libssl-dev libclang-dev -y || { echo "Gagal menginstal komponen dasar."; exit 1; }
+    sudo apt install pkg-config curl build-essential libssl-dev libclang-dev -y
 
-    # Memeriksa apakah Docker telah diinstal
-    if ! command -v docker &> /dev/null; then
-        echo "Docker tidak terdeteksi, sedang menginstal..."
+    # Memeriksa apakah Docker sudah terinstal
+    if ! command -v docker &> /dev/null
+    then
+        # Jika Docker belum terinstal, lakukan instalasi
+        echo -e "${YELLOW}Docker tidak terdeteksi, sedang menginstal...${NC}"
         sudo apt-get install ca-certificates curl gnupg lsb-release
 
         # Menambahkan kunci GPG resmi Docker
@@ -137,49 +140,47 @@ EOF
           "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
           $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-        # Memberikan izin pada file Docker
+        # Memberikan hak akses pada file Docker
         sudo chmod a+r /etc/apt/keyrings/docker.gpg
         sudo apt-get update
 
         # Menginstal versi terbaru Docker
-        sudo apt-get install docker-ce docker-ce-cli containerd.io -y || { echo "Gagal menginstal Docker."; exit 1; }
-        
-        # Menginstal Docker Compose
+        sudo apt-get install docker-ce docker-ce-cli containerd.io -y 
         DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
         mkdir -p $DOCKER_CONFIG/cli-plugins
         curl -SL https://github.com/docker/compose/releases/download/v2.25.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-        sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+        sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
         docker compose version
+        
     else
-        echo "Docker telah terinstal."
+        echo -e "${GREEN}Docker sudah terinstal.${NC}"
     fi
 
     # Menjalankan kontainer
-    echo "Menjalankan kontainer..."
-    docker compose up -d || { echo "Gagal menjalankan kontainer."; exit 1; }
+    docker compose up -d
 
-    echo "========================= Instalasi Selesai ==============================="
-    echo "Silakan gunakan cd infernet-node/deploy untuk masuk ke direktori dan kemudian gunakan docker compose logs -f untuk memeriksa log."
+    echo -e "${CYAN}========================= Instalasi Selesai ======================================${NC}"
+    echo "Silakan masuk ke direktori infernet-node/deploy, lalu gunakan perintah docker compose logs -f untuk memeriksa log."
 }
 
-# Memeriksa log node
+# Fungsi untuk memeriksa status log node
 function check_service_status() {
-    cd infernet-node/deploy || { echo "Gagal masuk ke direktori deploy."; exit 1; }
+    cd infernet-node/deploy
     docker compose logs -f
 }
 
 # Menu utama
 function main_menu() {
     clear
-    echo "Silakan pilih tindakan yang ingin dilakukan:"
-    echo "1. Instal node"
-    echo "2. Lihat log node"
+    echo -e "${BLUE}Silakan pilih tindakan yang ingin dilakukan:${NC}"
+    echo -e "${YELLOW}1. Instal Node${NC}"
+    echo -e "${YELLOW}2. Periksa Log Node${NC}"
     read -p "Masukkan pilihan (1-2): " OPTION
 
     case $OPTION in
     1) install_node ;;
     2) check_service_status ;;
-    *) echo "Pilihan tidak valid." ;;
+    *) echo -e "${RED}Pilihan tidak valid.${NC}" ;;
     esac
 }
 
