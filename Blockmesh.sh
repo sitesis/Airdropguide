4dae6e3e3512f3a9d76def1bd15b5b27be0fe474
Ahmad Choiruddin <83292508+choir94@users.noreply.github.com>
2024-11-10 09:44:42 +0700

    Update Blockmesh.sh

--- Blockmesh.sh
+++ Blockmesh.sh
@@ -80,7 +80,7 @@ version: '3.8'
 
 services:
   blockmesh-cli:
-    image: airdropnode/blockmesh-cli:v0.0.316
+    image: airdropnode/blockmesh-cli:v0.0.358
     container_name: blockmesh-cli
     environment:
       - USER_EMAIL=\${USER_EMAIL}
