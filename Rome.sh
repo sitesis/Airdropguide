79092d6ca94fa0e9ae0af8fe3a269232a20db120
Ahmad Choiruddin <83292508+choir94@users.noreply.github.com>
2024-11-13 22:05:34 +0700

    Update Rome.sh

--- Rome.sh
+++ Rome.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Skrip instalasi logo
+curl -s https://raw.githubusercontent.com/choir94/Airdropguide/refs/heads/main/logo.sh | bash
+sleep 5
+
 # Memeriksa apakah Node.js sudah terinstal
 if command -v node >/dev/null 2>&1; then
     echo "Node.js sudah terinstal: $(node -v)"
