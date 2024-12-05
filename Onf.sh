55b8638ef64c48986fd890436a033be81cad3e45
Ahmad Choiruddin <83292508+choir94@users.noreply.github.com>
2024-12-05 19:08:14 +0700

    Create Onf.sh

---
+++ Onf.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -e
+
+reset="\033[0m"
+cyan="\033[36m"
+green="\033[32m"
+red="\033[31m"
+VERSION=$(curl -sS https://raw.githubusercontent.com/OnFinality-io/onf-cli/master/VERSION)
+
+# Check OS
+if [ "$(uname)" = "Darwin" ]; then
+  LOCATION="/root/onf"
+  URL="https://github.com/OnFinality-io/onf-cli/releases/download/v$VERSION/onf-darwin-amd64-v$VERSION"
+  SYSTEM="MACOS"
+elif [ "$(uname -s)" = "Linux" ]; then
+  LOCATION="/root/onf"
+  URL="https://github.com/OnFinality-io/onf-cli/releases/download/v$VERSION/onf-linux-amd64-v$VERSION"
+  SYSTEM="LINUX"
+elif [ "$(uname -s)" = "MINGW64_NT" ]; then
+  LOCATION="%WINDIR%\system32"
+  URL="https://github.com/OnFinality-io/onf-cli/releases/download/v$VERSION/onf-windows-amd64-v$VERSION.exe"
+  SYSTEM="WINDOWS"
+fi
+
+# Download binary
+printf %s"$cyan> Downloading ...$reset\n"
+cd "$LOCATION"
+if curl -L "$URL" --output onf; then
+  if [ "$SYSTEM" != "WINDOWS" ]; then
+    chmod 773 onf
+  fi
+  printf %s"$green > onf command v$VERSION is ready. $reset\n"
+else
+  printf %s"$red> Failed to download $URL.$reset\n"
+  exit 1
+fi
