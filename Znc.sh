4471d826e3e6a24c6221b9c60dca4475c8212aa5
Ahmad Choiruddin <83292508+choir94@users.noreply.github.com>
2024-12-12 14:01:27 +0700

    Update Znc.sh

--- Znc.sh
+++ Znc.sh
@@ -1,160 +1,158 @@
 #!/bin/bash
 
-# Perbarui Paket Sistem
-echo -e "${YELLOW}Perbarui Paket Sistem...${NC}"
-sudo apt update && sudo apt upgrade -y
-sleep 5
-
-curl -s https://raw.githubusercontent.com/choir94/Airdropguide/refs/heads/main/logo.sh | bash
-sleep 5
-
-# Variabel warna untuk output
-YELLOW='\033[1;33m'
-GREEN='\033[1;32m'
-RED='\033[1;31m'
-NC='\033[0m'
-
 # Meminta nama validator dari pengguna
 read -p "Masukkan nama validator: " VALIDATOR_NAME
 
 # Memastikan sistem memiliki dependensi yang dibutuhkan
 install_dependencies() {
-    echo -e "${YELLOW}Memeriksa dan menginstal dependensi yang diperlukan...${NC}"
+    echo "Memeriksa dan menginstal dependensi yang diperlukan..."
     sudo apt-get update
-    sudo apt-get install -y jq curl git build-essential
+    if ! sudo apt-get install -y jq curl git build-essential; then
+        echo "Error: Gagal menginstal dependensi."
+        exit 1
+    fi
 }
 
 # Memastikan Docker dan Docker Compose terinstal
 check_docker_installation() {
     if ! command -v docker &>/dev/null; then
-        echo -e "${YELLOW}Docker tidak ditemukan. Menginstal Docker...${NC}"
-        sudo apt-get install -y docker.io
+        echo "Docker tidak ditemukan. Menginstal Docker..."
+        if ! sudo apt-get install -y docker.io; then
+            echo "Error: Gagal menginstal Docker."
+            exit 1
+        fi
         sudo systemctl start docker
         sudo systemctl enable docker
     else
-        echo -e "${GREEN}Docker sudah terinstal.${NC}"
+        echo "Docker sudah terinstal."
     fi
 
     if ! command -v docker-compose &>/dev/null; then
-        echo -e "${YELLOW}Docker Compose tidak ditemukan. Menginstal Docker Compose...${NC}"
-        sudo curl -L "https://github.com/docker/compose/releases/download/v2.17.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-        sudo chmod +x /usr/local/bin/docker-compose
+        echo "Docker Compose tidak ditemukan. Menginstal Docker Compose..."
+        if ! sudo curl -L "https://github.com/docker/compose/releases/download/v2.17.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose; then
+            echo "Error: Gagal mengunduh Docker Compose."
+            exit 1
+        fi
+        if ! sudo chmod +x /usr/local/bin/docker-compose; then
+            echo "Error: Gagal memberikan izin eksekusi untuk Docker Compose."
+            exit 1
+        fi
     else
-        echo -e "${GREEN}Docker Compose sudah terinstal.${NC}"
-    fi
-
-    # Menambahkan pengguna ke grup docker agar bisa menjalankan Docker tanpa sudo
-    if ! groups $USER | grep &>/dev/null '\bdocker\b'; then
-        echo -e "${YELLOW}Menambahkan pengguna ke grup docker untuk menjalankan perintah Docker tanpa sudo...${NC}"
-        sudo usermod -aG docker $USER
-        echo -e "${GREEN}Pengguna berhasil ditambahkan ke grup docker. Anda perlu logout dan login kembali agar perubahan berlaku.${NC}"
-    else
-        echo -e "${GREEN}Pengguna sudah berada di grup docker.${NC}"
+        echo "Docker Compose sudah terinstal."
     fi
 }
 
 # Memastikan direktori chain-data ada dan memiliki izin yang benar
 ensure_chain_data_directory() {
-    echo -e "${YELLOW}Memastikan direktori ./chain-data dan izin yang benar...${NC}"
+    echo "Memastikan direktori ./chain-data dan izin yang benar..."
+    if ! mkdir -p ./chain-data; then
+        echo "Error: Gagal membuat direktori ./chain-data."
+        exit 1
+    fi
 
-    # Direktori yang diperlukan
-    mkdir -p ./chain-data
+    if ! sudo chown -R $USER:$USER ./chain-data; then
+        echo "Error: Gagal mengubah kepemilikan direktori ./chain-data."
+        exit 1
+    fi
 
-    # Memperbaiki izin direktori
-    sudo chown -R $USER:$USER ./chain-data
-    sudo chmod -R 755 ./chain-data
-
-    echo -e "${GREEN}Direktori ./chain-data dan izin telah disiapkan.${NC}"
+    if ! sudo chmod -R 755 ./chain-data; then
+        echo "Error: Gagal memberikan izin yang benar untuk direktori ./chain-data."
+        exit 1
+    fi
+    echo "Direktori ./chain-data dan izin telah disiapkan."
 }
 
-# Membuat file docker-compose.yml di direktori saat ini
+# Membuat file docker-compose.yml untuk Production Setup
 create_docker_compose_file() {
-    echo -e "${YELLOW}Membuat file docker-compose.yml...${NC}"
+    echo "Membuat file docker-compose.yml untuk Production Setup..."
     cat <<EOF >./docker-compose.yml
 version: '3.8'
+
+# Production Setup
 services:
-  zenchain:
+  zenchain-prod:
     image: ghcr.io/zenchain-protocol/zenchain-testnet:latest
-    container_name: zenchain
+    platform: linux/amd64
+    container_name: zenchain-prod
     ports:
       - "9944:9944"
     volumes:
       - ./chain-data:/chain-data
-      - ./zenchain-config:/config
-    user: "${UID}:${GID}"  # Menambahkan user dan group ID sesuai dengan yang digunakan di host
-    command: >
-      ./usr/bin/zenchain-node
-      --base-path=/chain-data
-      --rpc-cors=all
-      --rpc-methods=unsafe
-      --unsafe-rpc-external
-      --validator
-      --name=${VALIDATOR_NAME}
-      --bootnodes=/dns4/node-7242611732906999808-0.p2p.onfinality.io/tcp/26266/p2p/12D3KooWLAH3GejHmmchsvJpwDYkvacrBeAQbJrip5oZSymx5yrE
-      --chain=zenchain_testnet
+    command:
+      - "./usr/bin/zenchain-node"
+      - "--base-path=/chain-data"
+      - "--rpc-cors=all"
+      - "--validator"
+      - "--name=${VALIDATOR_NAME}"
+      - "--bootnodes=/dns4/node-7242611732906999808-0.p2p.onfinality.io/tcp/26266/p2p/12D3KooWLAH3GejHmmchsvJpwDYkvacrBeAQbJrip5oZSymx5yrE"
+      - "--chain=zenchain_testnet"
 EOF
-    echo -e "${GREEN}File docker-compose.yml berhasil dibuat di direktori saat ini.${NC}\n"
+
+    echo "File docker-compose.yml untuk Production Setup berhasil dibuat di direktori saat ini."
 }
 
 # Memastikan berada di direktori yang benar sebelum menjalankan docker-compose
 run_docker_compose() {
-    echo -e "${YELLOW}Menjalankan Docker Compose untuk ZenChain Node...${NC}"
-
-    # Menyimpan direktori saat ini
+    echo "Menjalankan Docker Compose untuk ZenChain Node..."
     CURRENT_DIR=$(pwd)
 
-    # Memastikan kita berada di direktori yang sama dengan docker-compose.yml
     if [[ ! -f ./docker-compose.yml ]]; then
-        echo -e "${RED}File docker-compose.yml tidak ditemukan di direktori ini!${NC}"
+        echo "Error: File docker-compose.yml tidak ditemukan di direktori ini!"
         exit 1
     fi
 
-    # Menjalankan docker-compose dari direktori tempat file berada
-    docker-compose down
-    docker-compose up -d
-    echo -e "${GREEN}ZenChain Node berhasil dijalankan.${NC}"
+    if ! docker-compose down; then
+        echo "Error: Gagal menurunkan Docker Compose."
+        exit 1
+    fi
+
+    if ! docker-compose up -d; then
+        echo "Error: Gagal menjalankan Docker Compose."
+        exit 1
+    fi
+    echo "ZenChain Node berhasil dijalankan."
 }
 
 # Menunggu 60 detik untuk memastikan RPC aktif
 wait_for_rpc() {
-    echo -e "${YELLOW}Menunggu 60 detik untuk memastikan RPC aktif...${NC}"
+    echo "Menunggu 60 detik untuk memastikan RPC aktif..."
     sleep 60
 }
 
 # Mendapatkan session keys
 get_session_keys() {
-    echo -e "${YELLOW}Mendapatkan Session Keys...${NC}"
+    echo "Mendapatkan Session Keys..."
     SESSION_KEYS=$(curl --max-time 10 --silent --retry 5 --retry-delay 5 --url http://localhost:9944 -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"author_rotateKeys","params":[],"id":1}' | jq -r .result)
     if [[ -z "$SESSION_KEYS" ]]; then
-        echo -e "${YELLOW}Gagal mendapatkan Session Keys. Pastikan port 9944 terbuka.${NC}"
+        echo "Error: Gagal mendapatkan Session Keys. Pastikan port 9944 terbuka."
     else
-        echo -e "${GREEN}Session Keys berhasil didapatkan: ${SESSION_KEYS}${NC}"
+        echo "Session Keys berhasil didapatkan: ${SESSION_KEYS}"
         echo "$SESSION_KEYS" > session_keys.txt
-        echo -e "${GREEN}Session Keys disimpan di file session_keys.txt.${NC}"
+        echo "Session Keys disimpan di file session_keys.txt."
     fi
 }
 
 # Melihat logs dari kontainer Docker Zenchain
 view_logs() {
-    echo -e "${YELLOW}Melihat logs dari Docker container Zenchain...${NC}"
-    docker logs -f zenchain
+    echo "Melihat logs dari Docker container Zenchain..."
+    docker logs -f zenchain-prod
 }
 
 # Menanyakan apakah ingin melihat logs setelah setup selesai
 view_logs_option() {
-    echo -e "${YELLOW}Apakah Anda ingin melihat logs dari ZenChain Node? (y/n)${NC}"
+    echo "Apakah Anda ingin melihat logs dari ZenChain Node? (y/n)"
     read -p "Pilih opsi: " VIEW_LOGS
     if [[ "$VIEW_LOGS" == "y" || "$VIEW_LOGS" == "Y" ]]; then
         view_logs
     else
-        echo -e "${GREEN}Proses selesai. Session Keys telah disimpan di session_keys.txt.${NC}"
+        echo "Proses selesai. Session Keys telah disimpan di session_keys.txt."
     fi
 }
 
 # Menampilkan ajakan bergabung dengan Channel Telegram
 join_telegram_channel() {
-    echo -e "${GREEN}Terima kasih telah mengikuti proses ini. Jangan lupa untuk bergabung dengan Channel Telegram AirdropNode untuk update lebih lanjut: ${NC}"
-    echo -e "${GREEN}👉 ${YELLOW}https://t.me/airdrop_node${NC}"
+    echo "Terima kasih telah mengikuti proses ini. Jangan lupa untuk bergabung dengan Channel Telegram AirdropNode untuk update lebih lanjut:"
+    echo "👉 https://t.me/airdrop_node"
 }
 
 # Menjalankan semua langkah
